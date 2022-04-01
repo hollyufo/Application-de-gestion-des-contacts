18542de23066a9b9095b8f50f4ea79e29f0d75d4
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-04-01 16:28:20 +0000

    updated regex and js function

--- assets/js/validation2.js
+++ assets/js/validation2.js
@@ -1,29 +1,29 @@
 const username = document.getElementById('username')
 const password = document.getElementById('password')
 const form = document.getElementById('loginform')
-const passworderror = document.getElementById('passwordError')
 const usernameErrorField = document.getElementById('usernameError')
+const verifypassword = document.getElementById('verifypassword')
+const passworderrorfield = document.getElementById('passwordError')
 var valid
 // checking data for all inputs if it exist
 var index = 0
 const patterns = /^[a-z0-9_-]{3,15}$/
 form.addEventListener('submit', (e) => {
-    if (username.value === '' || username.value == null) {
+if (username.value === '' || username.value == null) {
         index++
         username.classList.add("is-invalid");
     }
     if(patterns.test(username.value) == false){
-        index++
         username.classList.add("is-invalid");
         usernameErrorField.innerText = "your username must be between 3 and 10 characters and alphanumeric only."
     }
-
     if (password.value === '' || password.value == null) {
         index++
         password.classList.add("is-invalid");
-        passworderror.innerText = "password is required"
+        passworderrorfield.innerText = "password is required"
     }
     if (index > 0) {
         e.preventDefault()
     }
-}) 
+
+})    
